d16b329fd1f2ae0e75f75a260523777bfce5a1b9
Matthijs Laan <matthijsln@gmail.com>
2009-11-23 13:04:25 +0000

    alleen rollback wanneer tx active

--- src/java/nl/b3p/gis/viewer/struts/BaseHibernateAction.java
+++ src/java/nl/b3p/gis/viewer/struts/BaseHibernateAction.java
@@ -4,19 +4,19 @@
  * works in cooperation with B3P Kaartenbalie.
  *
  * Copyright 2006, 2007, 2008 B3Partners BV
- * 
+ *
  * This file is part of B3P Gisviewer.
- * 
+ *
  * B3P Gisviewer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * B3P Gisviewer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with B3P Gisviewer.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -93,7 +93,9 @@ public abstract class BaseHibernateAction extends ExtendedMethodAction {
             tx.commit();
             return forward;
         } catch (Exception e) {
-            tx.rollback();
+            if(tx.isActive()) {
+				tx.rollback();
+			}
             log.error("Exception occured, rollback", e);
 
             if (e instanceof org.hibernate.JDBCException) {
