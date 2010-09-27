71bb22e5590538c88f632b70ec3d52af68770be9
Boy de Wit <boydewit@b3partners.nl>
2010-09-27 12:54:37 +0000

    - bij zoekveld nu ook inputzoekconfiguratie in te stellen - zit in JSON van ZoekAttribuut als inputZoekConfiguratie

--- database/104_create_zoeker_velden_PG.sql
+++ database/104_create_zoeker_velden_PG.sql
@@ -1,2 +1,3 @@
 ALTER TABLE zoekveld ADD COLUMN inputtype integer;
 ALTER TABLE zoekveld ADD COLUMN inputsize integer;
+ALTER TABLE zoekveld ADD COLUMN inputzoekconfiguratie integer;
--- database/104_create_zoeker_velden_oracle.sql
+++ database/104_create_zoeker_velden_oracle.sql
@@ -1,2 +1,3 @@
 alter table zoekveld add inputtype number(10,0);
 alter table zoekveld add inputsize number(10,0);
+alter table zoekveld add inputzoekconfiguratie number(10,0);
