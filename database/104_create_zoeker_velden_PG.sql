d610140747c6fdc45a7bdd0625411c2d48ee2020
Chris van Lith <chrisvanlith@gmail.com>
2010-09-28 18:49:08 +0000

    extra db velden voor zoekerconfiguratie

--- database/104_create_zoeker_velden_PG.sql
+++ database/104_create_zoeker_velden_PG.sql
@@ -1,3 +1,7 @@
 ALTER TABLE zoekveld ADD COLUMN inputtype integer;
 ALTER TABLE zoekveld ADD COLUMN inputsize integer;
 ALTER TABLE zoekveld ADD COLUMN inputzoekconfiguratie integer;
+
+ALTER TABLE zoekconfiguratie ADD COLUMN resultlistdynamic BOOLEAN;
+UPDATE zoekconfiguratie SET resultlistdynamic = true;
+ALTER TABLE zoekconfiguratie ALTER COLUMN resultlistdynamic SET NOT NULL;
--- database/104_create_zoeker_velden_oracle.sql
+++ database/104_create_zoeker_velden_oracle.sql
@@ -1,3 +1,7 @@
 alter table zoekveld add inputtype number(10,0);
 alter table zoekveld add inputsize number(10,0);
 alter table zoekveld add inputzoekconfiguratie number(10,0);
+
+ALTER TABLE zoekconfiguratie ADD (resultlistdynamic NUMBER(1,0));
+UPDATE zoekconfiguratie SET resultlistdynamic = 1;
+ALTER TABLE zoekconfiguratie MODIFY (resultlistdynamic NOT NULL);
