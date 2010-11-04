205ff2ec2d3e3d6bf58a0a8678bf6591ea7a78d1
Boy de Wit <boydewit@b3partners.nl>
2010-11-04 09:52:40 +0000

    Nieuwe gegevensbron mappings en script voor Postgres.

---
+++ database/105_create_gegevensbron_PG.sql
@@ -0,0 +1,31 @@
+create table gegevensbron (
+        id serial not null,
+        naam varchar(255),
+        bron int4,
+		admin_tabel varchar(255),
+        admin_pk varchar(255),
+		admin_query text,
+		parent int4,
+		admin_fk varchar(255),
+		admin_tabel_opmerkingen text,
+        primary key (id)
+    );
+	
+alter table gegevensbron 
+        add constraint FK_PARENT 
+        foreign key (parent) 
+        references clusters;
+		
+ALTER TABLE thema_data ADD COLUMN gegevensbron int4;
+
+alter table thema_data 
+        add constraint FK_GEGEVENSBRON 
+        foreign key (gegevensbron)
+        references gegevensbron;
+
+ALTER TABLE themas ADD COLUMN gegevensbron int4;
+
+alter table themas
+        add constraint FK_THEMAS_GEGEVENSBRON 
+        foreign key (gegevensbron)
+        references gegevensbron;
