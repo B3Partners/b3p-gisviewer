c4e3add678bad93a1efaf59919440c48c260fdb4
Boy de Wit <boydewit@b3partners.nl>
2010-11-22 13:51:27 +0000

    - rename + typo

--- database/202_create_voorzieningen_PG.sql
+++ database/202_create_voorzieningen_PG.sql
@@ -5,7 +5,7 @@ CREATE TABLE voorzieningen (
 	huisnummer integer,
 	postcode character varying(15), 
 	the_geom geometry,
-	CONSTRAINT vergunningen_pkey PRIMARY KEY (id),
+	CONSTRAINT voorzieningen_pkey PRIMARY KEY (id),
 	CONSTRAINT enforce_dims_the_geom CHECK (ndims(the_geom) = 2),
 	CONSTRAINT enforce_geotype_the_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
 	CONSTRAINT enforce_srid_the_geom CHECK (srid(the_geom) = 28992)
