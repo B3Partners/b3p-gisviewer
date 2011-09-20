9e547fd2dbb15f095926bbcae4f37c4d8d690321
Boy de Wit <boydewit@b3partners.nl>
2011-09-20 12:50:53 +0000

    - Oracle mapping voor Applicatie entity en create script.

---
+++ database/700_create_applicatie_oracle.sql
@@ -0,0 +1,15 @@
+create table applicatie (
+    id number(10,0) not null,
+    naam varchar2(255 char),
+    code varchar2(255 char),
+    gebruikers_code varchar2(255 char),
+    parent number(10,0),
+    datum_gebruikt timestamp not null,
+    read_only number(1) default 1 not null ,
+    user_copy number(1) default 0 not null,
+    versie number(10,0) default 1 not null,
+    default_app number(1) default 0 not null,
+    primary key (id)
+);
+
+create sequence applicatie_id_seq;
