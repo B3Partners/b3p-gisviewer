728c9c8a279ae1f4af26f0214a5b93df9e163397
Boy de Wit <boydewit@b3partners.nl>
2011-12-09 16:59:24 +0000

    - basis functionaliteit nieuwe cyclomedia api werkt.
    Nu nog netjes opslaan in db zodat je het kunt configureren per applicatie;

--- src/java/nl/b3p/gis/viewer/CyclomediaAction.java
+++ src/java/nl/b3p/gis/viewer/CyclomediaAction.java
@@ -2,11 +2,23 @@ package nl.b3p.gis.viewer;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Signature;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -30,10 +42,11 @@ public class CyclomediaAction extends ViewerCrudAction {
     private final String CERT_TYPE = "PKCS12";
     private final String KEY_FORMAT = "PKCS#8";
     private final String SIG_ALGORITHM = "SHA1withRSA";
-    
+    private final String URL_ENCODING = "utf-8";
 
     /**
      * This method is called when a user has clicked a link to a Cyclomedia image.
+     * /gisviewer/globespotter.do?imageId=[foto]
     */
     @Override
     public ActionForward unspecified(ActionMapping mapping, DynaValidatorForm dynaForm,
@@ -41,62 +54,117 @@ public class CyclomediaAction extends ViewerCrudAction {
 
         String imageId = (String) request.getParameter("imageId");
         
-        if (imageId != null && !imageId.equals("")) {            
-            /* Fetch Cyclomedia configuration items */            
+        if (imageId != null && !imageId.equals("")) {     
+            
+            /* Configureerbaar maken. Opslaan in CyclomediaAccount entity */
             // apikey
-            // accountid
+            // acountId
             // wachtwoord
-            // saved private key from uploaded file
-            
-            /* Wat Venray foto id's */
-            // 5D05VXT6, 5D05VXU4,5D05VXUE
-            imageId = "5D05VXT6";
-            
-            String accountId = "venray_intern"; // B3_develop
-            char[] password = "5000".toCharArray(); //***REMOVED***
-            
-            DateFormat df = new SimpleDateFormat("yyyy-M-d H:m");         
-            String date = df.format(new Date());
-            String token ="X" + accountId + "&" + imageId + "&" + date;
+            // privatekey input veld of eenmalig via pfx upload ophalen
             
             String b3pApiKey = "K3MRqDUdej4JGvohGfM5e78xaTUxmbYBqL0tSHsNWnwdWPoxizYBmjIBGHAhS3U1";
-            String venrayApiKey = "978e273a8b2f117a2c3156744b6fca0ebef1bedd";            
+            String accountId = "5155";
+            String wachtwoord = "5155";
+            
+            DateFormat df = new SimpleDateFormat("yyyy-M-d H:mm");         
+            String date = df.format(new Date());
+            String token ="X" + accountId + "&" + imageId + "&" + date + "Z";
            
-            File bestand = new File("C:\\tmp\\venray_5000.pfx");
+            /* Lees private key uit pfx bestand */
+            File bestand = new File("C:\\tmp\\stichtsevecht_5155.pfx");
+            String base64 = getBase64EncodedSignatureFromPfx(bestand, wachtwoord, token);
             
-            KeyStore ks = java.security.KeyStore.getInstance(CERT_TYPE);
-            ks.load(new FileInputStream(bestand), password);            
+            /* TID */   
+            String tid = URLEncoder.encode(token + "&" + base64, URL_ENCODING);
             
-            Enumeration<String> aliases = ks.aliases();
-            PrivateKey privateKey = null;
-            while (aliases.hasMoreElements()) {
-                String alias = aliases.nextElement();
-                
-                Key key = ks.getKey(alias, password);
-                String keyFormat = key.getFormat();
-                
-                if ( (key instanceof RSAPrivateCrtKeyImpl) && keyFormat.equals(KEY_FORMAT) ) {
-                    privateKey = (PrivateKey)key;
-                }
-            }
-                    
-            Signature instance = Signature.getInstance(SIG_ALGORITHM);
-            instance.initSign(privateKey);
-            instance.update(privateKey.getEncoded());
-            byte[] signature = instance.sign();
-            
-            Base64 encoder = new Base64();
-            String base64 = new String(encoder.encode(signature));
-            
-            /* Genereate TID */            
-            String tid = URLEncoder.encode(token + "&" + base64, "utf-8");
-            
-            /* Set TID and imageId for jsp */   
+            /* Set Apikey, imageId and TID on request for jsp */
+            request.setAttribute("apiKey", b3pApiKey);
             request.setAttribute("imageId", imageId);
-            request.setAttribute("apiKey", venrayApiKey);
-            request.setAttribute("tid", tid);    
+            request.setAttribute("tid", tid);
         }
-
+        
         return mapping.findForward(SUCCESS);
-    }  
+    }
+    
+    private PrivateKey getPrivateKeyFromPfxFile(File bestand, String password)
+            throws KeyStoreException, IOException, NoSuchAlgorithmException,
+            CertificateException, UnrecoverableKeyException {
+        
+        PrivateKey privateKey = null;
+        
+        KeyStore ks = java.security.KeyStore.getInstance(CERT_TYPE);
+        ks.load(new FileInputStream(bestand), password.toCharArray());            
+
+        Enumeration<String> aliases = ks.aliases();        
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+
+            Key key = ks.getKey(alias, password.toCharArray());
+            String keyFormat = key.getFormat();
+
+            if ( (key instanceof RSAPrivateCrtKeyImpl) && keyFormat.equals(KEY_FORMAT) ) {
+                privateKey = (PrivateKey)key;
+            }
+        }
+        
+        return privateKey;
+    }
+    
+    private String getBase64EncodedSignatureFromPfx(File bestand, String password, String token)
+            throws KeyStoreException, IOException, NoSuchAlgorithmException,
+            CertificateException, UnrecoverableKeyException, InvalidKeyException,
+            SignatureException {
+        
+        String base64 = null;
+
+        PrivateKey privateKey = getPrivateKeyFromPfxFile(bestand, password);
+        byte[] signature = sign(privateKey, token);    
+
+        /* Base 64 encode signature */
+        Base64 encoder = new Base64();
+        base64 = new String(encoder.encode(signature));
+        
+        return base64;
+    }
+    
+    private byte[] sign(PrivateKey privateKey, String token) 
+            throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        
+        Signature instance = Signature.getInstance(SIG_ALGORITHM);
+        instance.initSign(privateKey);
+        instance.update(token.getBytes());
+        byte[] signature = instance.sign();
+        
+        return signature;
+    }
+    
+    private String getBase64EncodedPrivateKey(PrivateKey privateKey) {
+        String base64EncodedPrivateKey = null;
+        Base64 encoder = new Base64();
+        
+        base64EncodedPrivateKey = new String(encoder.encode(privateKey.getEncoded()));
+        
+        return base64EncodedPrivateKey;
+    }
+    
+    private String getTIDFromBase64EncodedString(String base64Encoded, String token)
+            throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException,
+            SignatureException, UnsupportedEncodingException {
+        
+        String tid = null;        
+        Base64 encoder = new Base64();
+        
+        byte[] tempBytes = encoder.decode(base64Encoded.getBytes());
+        
+        KeyFactory rsaKeyFac = KeyFactory.getInstance("RSA");  
+        PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(tempBytes);  
+        RSAPrivateKey privKey = (RSAPrivateKey)rsaKeyFac.generatePrivate(encodedKeySpec);          
+
+        byte[] signature = sign(privKey, token);
+
+        String base64 = new String(encoder.encode(signature));
+        tid = URLEncoder.encode(token + "&" + base64, URL_ENCODING);
+        
+        return tid;
+    }
 }
---
+++ src/java/nl/b3p/gis/viewer/db/CyclomediaAccount.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2011 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.gis.viewer.db;
+
+/**
+ *
+ * @author Boy de Wit
+ */
+public class CyclomediaAccount {
+    
+    private Integer id;
+    private String apiKey;
+    private String accountId;
+    private String wachtwoord;
+    private String privateBase64Key;
+    private String appCode;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPrivateBase64Key() {
+        return privateBase64Key;
+    }
+
+    public void setPrivateBase64Key(String privateBase64Key) {
+        this.privateBase64Key = privateBase64Key;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }   
+}
