c18b749c21625a80511736bfefd51eb8513f4aaa
Boy de Wit <boydewit@b3partners.nl>
2012-12-17 10:37:27 +0000

    - added oracle scripts;

---
+++ database/update 3.6 to 4.0/40_add_a11y_velden_oracle.sql
@@ -0,0 +1,3 @@
+ALTER TABLE zoekconfiguratie ADD COLUMN omschrijving clob;
+ALTER TABLE zoekveld ADD COLUMN omschrijving clob;
+ALTER TABLE resultaatveld ADD COLUMN omschrijving clob;
---
+++ database/update 3.6 to 4.0/50_tekstblok_add_kleur_oracle.sql
@@ -0,0 +1,1 @@
+ALTER TABLE tekstblok ADD COLUMN kleur varchar2(255 char);
