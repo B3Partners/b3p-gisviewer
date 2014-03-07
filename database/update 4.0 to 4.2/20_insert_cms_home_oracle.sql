0e841c9d485846fa0f833130b6a085cb0402ee97
Boy de Wit <boydewit@b3partners.nl>
2014-03-07 15:14:08 +0000

    Nog twee sequences aanpassen.

--- database/update 4.0 to 4.2/20_insert_cms_home_oracle.sql
+++ database/update 4.0 to 4.2/20_insert_cms_home_oracle.sql
@@ -14,6 +14,12 @@ insert into cms_pagina (id, titel, tekst, show_plain_map_btn, cdate, cms_menu, l
 values
 (1, 'Home', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ultrices est vitae vehicula. Morbi nulla lectus, posuere varius viverra vitae, sodales eget justo. Vestibulum fermentum mollis odio, ut sollicitudin elit luctus dictum. Nunc pellentesque a odio non pellentesque. Curabitur volutpat nisl sed semper cursus. Aliquam nibh est, pulvinar vitae neque et, aliquam gravida nisi. Sed ante est, ullamcorper vitae tincidunt vitae, interdum ac nisi.', 0, SYSDATE, 1, 0);
 
--- re create sequence for cms_menuitem after insert
+-- re create sequences
 drop sequence CMS_MENUITEM_ID_SEQ;
 create sequence CMS_MENUITEM_ID_SEQ INCREMENT BY 1 START WITH 2;
+
+drop sequence CMS_MENU_ID_SEQ;
+create sequence CMS_MENU_ID_SEQ INCREMENT BY 1 START WITH 2;
+
+drop sequence CMS_PAGINA_ID_SEQ;
+create sequence CMS_PAGINA_ID_SEQ INCREMENT BY 1 START WITH 2;
