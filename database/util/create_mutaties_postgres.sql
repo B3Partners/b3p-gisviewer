0d7b55ad13be0dbc069deb247497bc35ed27bb9e
Boy de Wit <boydewit@b3partners.nl>
2014-07-15 15:12:06 +0000

    Added create script mutaties for Oracle. Updated postgres version

--- database/util/create_mutaties_postgres.sql
+++ database/util/create_mutaties_postgres.sql
@@ -1,20 +1,20 @@
--- DROP TABLE mutaties;
-
+-- postGIS voorbeeld mutaties tabel
 CREATE TABLE mutaties (
-  	fid serial NOT NULL,
-  	the_geom geometry,
-	type character varying(255),
+  	id serial NOT NULL,  	
+	soort character varying(255),
 	status character varying(255),
 	naam_melder character varying(255),
-	sld_code character varying(255),
-  	CONSTRAINT mutaties_pk PRIMARY KEY (fid),
+	email_melder character varying(255),
+	opmerking text,
+	the_geom geometry,
+  	CONSTRAINT mutaties_pk PRIMARY KEY (id),
   	CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
   	CONSTRAINT enforce_geotype_the_geom CHECK (geometrytype(the_geom) = 'POLYGON'::text OR the_geom IS NULL),
   	CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 28992)
 );
 
-ALTER TABLE mutaties OWNER TO gouda;
+-- Goedzetten owner van tabel
+ALTER TABLE mutaties OWNER TO username;
 
--- DROP INDEX spatial_mutaties_the_geom;
-
+-- Aanmaken spatial index
 CREATE INDEX spatial_mutaties_the_geom ON mutaties USING gist (the_geom);
